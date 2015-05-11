ed6118e08dcbf52056b6470a5fe4129538c2f00f
Albin Svensson <albin.svensson@trialbee.com>
2015-05-11 16:17:43 +0200

    name in logo

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,17 +1,17 @@
 module ProjectsHelper
-  def print_dev_track(dev_presenter)
-    content_tag :div, class: 'dev-track', style: "width: #{dev_presenter.width}px" do
-      safe_join([print_time_line(dev_presenter)] + dev_presenter.features.map do |feature|
-        print_feature_block(feature, dev_presenter.efficiency)
+  def print_dev_track(developer)
+    content_tag :div, class: 'dev-track', style: "width: #{developer.width}px" do
+      safe_join([print_time_line(developer)] + developer.features.map do |feature|
+        print_feature_block(feature, developer.efficiency)
       end)
     end
   end
 
-  def print_time_line(dev_presenter)
+  def print_time_line(developer)
     content_tag :div, class: 'time-line' do
       safe_join([
         content_tag(:span, Date.today, class: 'time-start'),
-        content_tag(:span, Date.today + (dev_presenter.expected_length).days, class: 'time-stop')
+        content_tag(:span, Date.today + (developer.expected_length).days, class: 'time-stop')
         ])
     end
   end
